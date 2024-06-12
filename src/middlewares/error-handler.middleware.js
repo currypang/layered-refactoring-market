a66d61730b3011f1f401c70d56e4a3e604d8034f
romesway <romesway@gmail.com>
2024-06-13 00:49:04 +0900

    refactor/errorHandleMiddleware

--- src/middlewares/error-handler.middleware.js
+++ src/middlewares/error-handler.middleware.js
@@ -1,19 +1,37 @@
 import { HTTP_STATUS } from '../constants/http-status.constant.js';
 import { MESSAGES } from '../constants/message.constant.js';
 import { HttpError } from '../errors/http.error.js';
-// 본문에선 next넘겨주는 구문 없어도 사용해야 미들웨어로 받아올 수 있다. -> next 없으면 미들웨어로 인식하지 않는듯?
+
+// 에러 처리 미들웨어. HttpError 클래스를 이용해 생성된 인스턴스로 처리된 에러들을 모아서 응답 하도록 리팩토링.
+
 // eslint-disable-next-line no-unused-vars
 export const errorHandler = (err, req, res, next) => {
   // 조이 유효성 검사 처리
   if (err.isJoi) {
     return res.status(HTTP_STATUS.BAD_REQUEST).json({ status: HTTP_STATUS.BAD_REQUEST, message: err.message });
   }
-  // 회원가입 시 이메일 중복 처리
-  if (err.message === 'existUser') {
-    return res
-      .status(HTTP_STATUS.CONFLICT)
-      .json({ status: HTTP_STATUS.CONFLICT, message: MESSAGES.AUTH.COMMON.EMAIL.DUPLICATED });
+  // HttpError 인스턴스로 에러 처리
+  if (err instanceof HttpError.BadRequest) {
+    return res.status(err.status).json({ status: err.status, message: err.message });
   }
+  if (err instanceof HttpError.Unauthorized) {
+    return res.status(err.status).json({ status: err.status, message: err.message });
+  }
+  if (err instanceof HttpError.Forbidden) {
+    return res.status(err.status).json({ status: err.status, message: err.message });
+  }
+  if (err instanceof HttpError.NotFound) {
+    return res.status(err.status).json({ status: err.status, message: err.message });
+  }
+  if (err instanceof HttpError.Conflict) {
+    return res.status(err.status).json({ status: err.status, message: err.message });
+  }
+  if (err instanceof HttpError.InternalServerError) {
+    return res.status(err.status).json({ status: err.status, message: err.message });
+  }
+
+  // 나머지 에러는 API 리팩토링 하면서 정리
+
   // auth 로그인 이메일로 조회안되거나 비밀번호가 일치하지 않을 경우
   if (err === 'invalidSignIn') {
     return res
