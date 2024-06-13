14399c4ae802db6f5fe6ea2d4ae79c12f37f8fa9
romesway <romesway@gmail.com>
2024-06-13 16:14:22 +0900

    refactor/getResumeListAPI

--- src/controllers/resumes.controller.js
+++ src/controllers/resumes.controller.js
@@ -5,6 +5,7 @@ export class ResumesController {
   constructor(resumesService) {
     this.resumesService = resumesService;
   }
+  // 이력서 생성
   createResume = async (req, res, next) => {
     try {
       const { title, content } = req.body;
@@ -14,6 +15,21 @@ export class ResumesController {
       return res
         .status(HTTP_STATUS.OK)
         .json({ status: HTTP_STATUS.OK, message: MESSAGES.RESUMES.CREATE.SUCCEED, data: resume });
+    } catch (err) {
+      next(err);
+    }
+  };
+  // 이력서 목록 조회
+  getAllResumes = async (req, res, next) => {
+    try {
+      const { id, role } = req.user;
+      const sort = req.query.sort?.toLocaleLowerCase() === 'asc' ? 'asc' : 'desc';
+      const status = req.query.status || '';
+
+      const resumeList = await this.resumesService.getAllResumes(id, role, sort, status);
+      return res
+        .status(HTTP_STATUS.OK)
+        .json({ status: HTTP_STATUS.OK, message: MESSAGES.RESUMES.READ_LIST.SUCCEED, data: resumeList });
     } catch (err) {
       next(err);
     }
--- src/services/resumes.service.js
+++ src/services/resumes.service.js
@@ -1,9 +1,33 @@
+import { USER_CONS } from '../constants/user.constant.js';
+
 export class ResumesService {
   constructor(resumesRepository) {
     this.resumesRepository = resumesRepository;
   }
+  // 이력서 생성 로직
   createResume = async (title, content, userId) => {
     const resume = await this.resumesRepository.createResume(title, content, userId);
     return resume;
+  };
+  // 이력서 목록 조회 로직
+  getAllResumes = async (id, role, sort, status) => {
+    const condition = {
+      ...(role !== USER_CONS.RECRUITER && { authorId: id }),
+      ...(status && { status }),
+    };
+    let resumeList = await this.resumesRepository.getAllResumes(condition, sort);
+
+    resumeList = resumeList.map((resume) => {
+      return {
+        id: resume.id,
+        authorName: resume.author.name,
+        title: resume.title,
+        content: resume.content,
+        status: resume.status,
+        createdAt: resume.createdAt,
+        updatedAt: resume.updatedAt,
+      };
+    });
+    return resumeList;
   };
 }
